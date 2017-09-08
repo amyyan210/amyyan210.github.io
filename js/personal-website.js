adefd3e9856c98c29fbf1a2136d8eada3719fcc7
amyyan210 <amyyan210@gmail.com>
2017-09-07 19:58:14 -0500

    Continue with website development
    * Fix project page alignment
    * Auto resize personal photo using js
    * Change button behavior on project page according to screen size

--- js/personal-website.js
+++ js/personal-website.js
@@ -6,4 +6,17 @@ $("#download").click(function () {
     window.open("https://docs.google.com/document/d/e/2PACX-1vTY5Ee_VJIzsFE0sWrrQ7iGiIzVmSD1b4EDJyidKgGlj1pbAg0LxGk_gkZOX8Yy-kNpSLNz-hu5diUe/pub", "_blank");
 });
 
+var photoResize = function(){
+    if (window.innerWidth >= 1024) {
+        var h = window.innerHeight - $(".personal-photo").offset().top - 30;
+        $(".personal-photo").css("max-height", h + "px");
+    } else {
+        $(".personal-photo").css("max-height", "");
+
+    }
+};
+
+$(document).ready(photoResize);
+$(window).resize(photoResize);
+
 })();
